851e5901d9944aa5b5f58e697f7a02641e2161e1
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2023-10-06 20:59:06 +0800

    Update

---
+++ resources/views/pages/backend/__includes/sidebar-item.blade.php
@@ -0,0 +1,33 @@
+<li class="menu-section ">
+  <h4 class="menu-text"> MAIN </h4>
+  <i class="menu-icon ki ki-bold-more-hor icon-md"></i>
+</li>
+<li class="menu-item menu-item-submenu {{ (request()->is('dashboard/families*')) ? 'menu-item-open' : '' }}">
+  <a href="javascript:;" class="menu-link menu-toggle">
+    <i class="menu-icon fas fa-list"></i>
+    <span class="menu-text"> Families </span>
+    <i class="menu-arrow"></i>
+  </a>
+  <div class="menu-submenu">
+    <ul class="menu-subnav">
+      <li class="menu-item menu-item-submenu {{ (request()->is('dashboard/families/members*')) ? 'menu-item-active' : '' }}"><a href="{{ url('/dashboard/families/members') }}" class="menu-link"><span class="menu-text"><i class="menu-bullet menu-bullet-dot"><span></span></i> Members </span></a></li>
+    </ul>
+  </div>
+</li>
+
+<li class="menu-section ">
+  <h4 class="menu-text"> SCHEDULES </h4>
+  <i class="menu-icon ki ki-bold-more-hor icon-md"></i>
+</li>
+<li class="menu-item menu-item-submenu {{ (request()->is('dashboard/events*')) ? 'menu-item-open' : '' }}">
+  <a href="javascript:;" class="menu-link menu-toggle">
+    <i class="menu-icon fas fa-list"></i>
+    <span class="menu-text"> Events </span>
+    <i class="menu-arrow"></i>
+  </a>
+  <div class="menu-submenu">
+    <ul class="menu-subnav">
+      <li class="menu-item menu-item-submenu {{ (request()->is('dashboard/events/content-challenges*')) ? 'menu-item-active' : '' }}"><a href="{{ url('/dashboard/events/content-challenges') }}" class="menu-link"><span class="menu-text"><i class="menu-bullet menu-bullet-dot"><span></span></i> Content Challenges </span></a></li>
+    </ul>
+  </div>
+</li>
--- resources/views/pages/backend/__includes/sidebar.blade.php
+++ resources/views/pages/backend/__includes/sidebar.blade.php
@@ -4,22 +4,7 @@
 
       <li class="menu-item {{ (request()->is('dashboard')) ? 'menu-item-active' : '' }}"><a href="{{ url('/dashboard') }}" class="menu-link"><i class="menu-icon fas fa-desktop"></i><span class="menu-text"> Dashboard </span></a></li>
 
-      <li class="menu-section ">
-        <h4 class="menu-text"> MAIN </h4>
-        <i class="menu-icon ki ki-bold-more-hor icon-md"></i>
-      </li>
-      <li class="menu-item menu-item-submenu {{ (request()->is('dashboard/families*')) ? 'menu-item-open' : '' }}">
-        <a href="javascript:;" class="menu-link menu-toggle">
-          <i class="menu-icon fas fa-list"></i>
-          <span class="menu-text"> Families </span>
-          <i class="menu-arrow"></i>
-        </a>
-        <div class="menu-submenu">
-          <ul class="menu-subnav">
-            <li class="menu-item menu-item-submenu {{ (request()->is('dashboard/families/members*')) ? 'menu-item-active' : '' }}"><a href="{{ url('/dashboard/families/members') }}" class="menu-link"><span class="menu-text"><i class="menu-bullet menu-bullet-dot"><span></span></i> Members </span></a></li>
-          </ul>
-        </div>
-      </li>
+      @include('pages.backend.__includes.sidebar-item')
 
       <li class="menu-section ">
         <h4 class="menu-text"> EXTENSIONS </h4>
