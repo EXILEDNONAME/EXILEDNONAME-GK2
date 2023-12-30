d8cc8ee17b7b246b146620b162608e31b82168b7
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2023-12-30 22:42:32 +0800

    U#68 - Update Sidebar

--- resources/views/pages/backend/__includes/sidebar-item.blade.php
+++ resources/views/pages/backend/__includes/sidebar-item.blade.php
@@ -1,24 +1,13 @@
-<!-- <li class="menu-item menu-item-submenu {{ (request()->is('dashboard/schedules*')) ? 'menu-item-open' : '' }}">
-  <a href="javascript:void(0);" class="menu-link menu-toggle">
-    <i class="menu-icon fas fa-calendar-check"></i>
-    <span class="menu-text"> Schedules </span>
-    <i class="menu-arrow"></i>
-  </a>
-  <div class="menu-submenu">
-    <ul class="menu-subnav">
-      <li class="menu-item menu-item-submenu {{ (request()->is('dashboard/schedules/events*')) ? 'menu-item-active' : '' }}"><a href="{{ url('/dashboard/schedules/events') }}" class="menu-link"><span class="menu-text"><i class="menu-bullet menu-bullet-dot"><span></span></i> Events </span></a></li>
-      <li class="menu-item menu-item-submenu {{ (request()->is('dashboard/schedules/pk*')) ? 'menu-item-active' : '' }}"><a href="javascript:void(0);" onClick="alert('Maintenance!')" class="menu-link"><span class="menu-text"><i class="menu-bullet menu-bullet-dot"><span></span></i> PK </span></a></li>
-    </ul>
-  </div>
-</li> -->
 <li class="menu-item {{ (request()->is('dashboard/search-members*')) ? 'menu-item-active' : '' }}"><a href="{{ url('/dashboard/search-members') }}" class="menu-link"><i class="menu-icon fas fa-search"></i><span class="menu-text"> Search Members </span></a></li>
 
 <li class="menu-section ">
   <h4 class="menu-text"> MAIN </h4>
   <i class="menu-icon ki ki-bold-more-hor icon-md"></i>
 </li>
+@role('master-administrator|administrator|user-event')
 <li class="menu-item {{ (request()->is('dashboard/events*')) ? 'menu-item-active' : '' }}"><a href="{{ url('/dashboard/events') }}" class="menu-link"><i class="menu-icon fas fa-list-ul"></i><span class="menu-text"> Events </span></a></li>
-@role('master-administrator|administrator')
+@endrole
+@role('master-administrator')
 <li class="menu-item {{ (request()->is('dashboard/e-commerce*')) ? 'menu-item-active' : '' }}"><a href="javascript:void(0);" onClick="alert('Maintenance!')" class="menu-link"><i class="menu-icon fas fa-balance-scale"></i><span class="menu-text"> E-Commerce </span></a></li>
 <li class="menu-item {{ (request()->is('dashboard/invoices*')) ? 'menu-item-active' : '' }}"><a href="javascript:void(0);" onClick="alert('Maintenance!')" class="menu-link"><i class="menu-icon fas fa-list-ul"></i><span class="menu-text"> Invoices </span></a></li>
 @endrole
